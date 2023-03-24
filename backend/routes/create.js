1cdc44fd19ab55732fcbba7231b9a078d0a12542
Maurice Schorn <pxlmo.codes@gmail.com>
2023-03-24 10:46:54 +0100

    prevent duplicate short urls

--- backend/routes/create.js
+++ backend/routes/create.js
@@ -6,23 +6,24 @@ const router = express.Router();
 
 // example.com/create?url=api.example.com
 router.post("/", async (req, res) => {
-  let url;
-  const result = await URL.findOne({ original: req.query.url });
-  if (result != null) url = result.shortened;
+  const originalURL = await URL.findOne({ original: req.query.url });
+  if (originalURL != null) res.json({ url: result.shortened });
+  
+  let shortened = nanoid(8);
+  const shortenedURL = await URL.findOne({ shortened });
 
-  if (result == null) {
-    const shortened = nanoid(8);
-    url = shortened;
+  do {
+    shortened = nanoid(8);
+  } while (shortenedURL != null);
+  
+  const entry = new URL({
+    shortened,
+    original: req.query.url,
+  });
 
-    const entry = new URL({
-      shortened,
-      original: req.query.url,
-    });
+  await entry.save();
 
-    await entry.save();
-  }
-
-  res.json({ url });
+  res.json({ url: shortened });
 });
 
 export default router;
